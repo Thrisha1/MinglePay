e621b999ad368621100ba390f9e1665dc10d3e32
Thrisha2 <smallbird309@gmail.com>
2023-04-13 23:11:27 +0530

    header set

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Logo from '../assets/logo.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
+import '../app/globals.css'
+import { UsersIcon } from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className ="flex items-center shadow-lg justify-between px-3">
+      <div className="flex items-center">
+        <Image src={Logo} alt="Logo" width="70" height="70" />
+        <Link href="/" >
+          <h1 className="text-blue-500 text-2xl font-sans">MINGLEPAY</h1>
+        </Link>
+      </div>
+      <div className="">
+        <UsersIcon class="h-6 w-6 " />
+      </div>
+      
+    </div>
   )
 }
 
