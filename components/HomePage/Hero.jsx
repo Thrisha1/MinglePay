a6c6e0b710f3f4696c2c5e8a0ba4dba066f8abc0
naaji-navas <najidnavas2000@gmail.com>
2023-04-14 10:37:40 +0530

    added the google login and hero

--- components/HomePage/Hero.jsx
+++ components/HomePage/Hero.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (<div>
-      <section className="bg-gray-50 min-h-[600px] flex  justify-center">
-        <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+      <section className=" min-h-[600px] flex  justify-center">
+        <div className="bg-gradient-to-br mx-5 from-[#86fde8] mt-5 to-[#acb6e5] rounded-xl overflow-hidden flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
           <div className="md:w-1/2 px-8 md:px-16">
-            <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
-            <p className="text-xs mt-4 text-[#002D74]">If you are already a member, easily log in</p>
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">Earn money <br/> by sharing <br/> WhatsApp status</h1>
+            <p className="mb-4">With MinglePay, you can earn money by sharing branded content from premium brands in your
+              WhatsApp status. That's right, all you have to do is share and earn.</p>
             <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]">
               <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
