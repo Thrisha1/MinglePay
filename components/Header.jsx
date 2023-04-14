f8fa7550fa3f0e45d58aa9b0326b5ab3825227ad
Thrisha2 <smallbird309@gmail.com>
2023-04-14 22:05:07 +0530

    View all ads set

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           <h1 className="text-blue-500 text-2xl font-sans">MINGLEPAY</h1>
         </Link>
       </div>
-      <Link href="/" className="">
+      <Link href="/profile" className="">
         <UsersIcon class="h-6 w-6 " />
       </Link>
       
