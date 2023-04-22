38a26c25437313bcd7787cb05115ea64ed054612
Thrisha2 <smallbird309@gmail.com>
2023-04-22 18:01:49 +0530

    footer set

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -13,9 +13,10 @@ const page = () => {
   // const [startDate, setStartDate] = useState(new Date());
 
   return (
-    <div className="p-5 w-100 bg-[#101010]" style={{backgroundImage:`url(${bg})`,
+    <div className=" w-100 " style={{backgroundImage:`url(${bg})`,
     backgroundRepeat: 'no-repeat',
                                       }} >
+    <div className="bg-[#101010] rounded-b-xl px-3  py-2">
       <div className="flex flex-col h-[38rem] justify-between">
         <div className="">
           <p className="text-xl tracking-wider text-white">User Profile</p>
@@ -25,31 +26,31 @@ const page = () => {
         <Details/>
         
       </div>
-      <Banner/>
-      <hr />
-      <div className="my-2 flex flex-col h-72 justify-between">
+      </div>
+      <div className="my-2 flex flex-col h-64 justify-between px-4">
           <h1 className="text-green-500">MORE LINKS</h1>
           <div className="">
-          <Link href="/profile" className="text-white text-md">How does it work?</Link>
+          <Link href="/profile" className="text-black text-md">How does it work?</Link>
           </div>
           <div className="">
-          <Link href="/profile" className="text-white text-md">Chat with us</Link>
+          <Link href="/profile" className="text-black text-md">Chat with us</Link>
           </div>
           <div className="">
-          <Link href="/profile" className="text-white text-md">Join Unizone Community on Telegram</Link>
+          <Link href="/profile" className="text-black text-md">Join Unizone Community on Telegram</Link>
           </div>
           <div className="">
-          <Link href="/profile" className="text-white text-md">Frequently Asked Questions</Link>
+          <Link href="/profile" className="text-black text-md">Frequently Asked Questions</Link>
           </div>
           <div className="">
-          <Link href="/profile" className="text-white text-md">Terms of Service</Link>
+          <Link href="/profile" className="text-black text-md">Terms of Service</Link>
           </div>
           <div className="">
-          <Link href="/profile" className="text-white text-md">Privacy Policy</Link>
+          <Link href="/profile" className="text-black text-md">Privacy Policy</Link>
           </div>
       </div>
+      
       <hr />
-      <h1 className="text-red-500 my-5 text-lg">Logout</h1>
+      <h1 className="text-red-500 m-5 text-lg">Logout</h1>
     </div>
   )
 }
