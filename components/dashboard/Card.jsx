afc5754fc17dac7f418de8e2ab6021efecc30d8d
DareDevilStudios <studios.daredevil@gmail.com>
2023-04-23 16:02:32 +0530

    dashoard completed and footer need to be done

---
+++ components/dashboard/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-10/12'>
+        hello
+    </div>
+  )
+}
+
+export default Card
--- components/dashboard/Dashboard.jsx
+++ components/dashboard/Dashboard.jsx
@@ -3,17 +3,14 @@ import Image from 'next/image'
 import Banner from '../../assets/banner3.gif'
 import BalanceCard from './BalanceCard'
 import RecentAds from './RecentAds'
+import Hero from './hero'
+import Link_cards from './Link_cards'
 
 const Dashboard = () => {
     return (
-        <section class="bg-white">
-            <div class="pt-4 px-4 max-w-2xl lg:py-5">
-                <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">👋 Hey Ameen! Welcome back.</h2>
-            </div>
-            <div class="px-4 w-full">
-                <Image src={Banner} className="h-[12rem] rounded-xl"  alt="Ameen" />
-            </div>
-            <BalanceCard/>
+        <section class="bg-gray-200 flex-col items-center justify-center w-full">
+            <Hero/>
+            <Link_cards/>
             <RecentAds/>
         </section>
     )
