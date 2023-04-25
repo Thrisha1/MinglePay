67bc3e1b9f5157f0c923538ea13366e3555aa452
DareDevilStudios <studios.daredevil@gmail.com>
2023-04-25 14:59:36 +0530

    ads page complete and footer minors changed

---
+++ components/ad-details/Ad_detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './hero'
+import More_Ads from './More_ads'
+
+const Ad_detail = () => {
+    return (
+        <section class="bg-gray-200 flex-col w-full">
+            <Hero/>
+            <More_Ads />
+        </section>
+    )
+}
+
+export default Ad_detail
