50bf3b2dbeb4d4019a5df2f9da407d372a525174
DareDevilStudios <studios.daredevil@gmail.com>
2023-04-28 22:42:41 +0530

    some change of array ads

--- components/dashboard/RecentAds.jsx
+++ components/dashboard/RecentAds.jsx
@@ -3,16 +3,46 @@ import AdsCard from './AdsCard'
 import ads from '../../assets/ads.jpeg'
 import Link from 'next/link'
 
+
 const RecentAds = () => {
+
+    const ads = [
+        {
+            id: 1,
+            image: Ad1,
+            title: 'Eid',
+            description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
+            amt: "1000"
+        },
+        {
+            id: 2,
+            image: Ad2,
+            title: 'Sale',
+            description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
+            amt: "1000"
+        },
+        {
+            id: 3,
+            image: Ad3,
+            title: 'Dress',
+            description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
+            amt: "1000"
+        }
+    ]
+
     return (
         <div className='px-3 my-5 bg-black py-8 flex-col justify-center items-center'>
             <div class="rounded-lg mb-4 flex justify-center">
                 <h1 class="text-2xl font-bold text-white text-center">NEW ADS</h1>
             </div>
             <div className='flex gap-10 overflow-x-scroll'>
-                <AdsCard image={ads} title="Quantity Surveying Course" amt="20" />
-                <AdsCard image={ads} title="Quantity Surveying Course" amt="20" />
-                <AdsCard image={ads} title="Quantity Surveying Course" amt="20" />
+
+                {
+                    ads.map(ad =>
+                        <AdsCard image={ads} title="Quantity Surveying Course" amt="20" />
+                    )
+                }
+                
             </div>
             <Link href="/ads-available" class="block w-full mt-6 text-center text-lg font-bold text-yellow-200">View All Ads</Link>
             <h1 className="text-gray-400 text-md mt-4 text-center flex justify-center">
