8412fedb41b80bdc5848fe3327c1a1bb5cf8c598
naaji-navas <najidnavas2000@gmail.com>
2023-05-10 15:45:44 +0530

    completed the refferral ui

--- components/ad-details/Ad_detail.jsx
+++ components/ad-details/Ad_detail.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import Hero from './hero'
 import More_Ads from './More_ads'
-import Ad1 from '/public/images/eid_ads.svg'
-import Ad2 from '/public/images/ad2.svg'
-import Ad3 from '/public/images/ad3.svg'
+import Ad1 from '@/public/images/eid_ads.svg'
+// import Ad1 from '/public/images/eid_ads.svg'
+import Ad2 from '@/public/images/ad2.svg'
+import Ad3 from '@/public/images/ad3.svg'
 
 const Ad_detail = () => {
 
     const ads = [
         {
             id: 1,
-            image: "@/images/eid_ads.svg",
+            image: Ad1,
             title: 'Eid',
             description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
             amt: "1000"
         },
         {
             id: 2,
-            image: "@/images/ad2.svg",
+            image: Ad2,
             title: 'Sale',
             description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
             amt: "1000"
         },
         {
             id: 3,
-            image: "@/images/ad3.svg",
+            image: Ad3,
             title: 'Dress',
             description: 'Eid is a digital advertising platform that helps you find the right ad for your business.',
             amt: "1000"
